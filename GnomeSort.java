85e960c09714b1fc6308306201bc442bc53d6d0f
DijanU <156262584+DijanU@users.noreply.github.com>
2024-02-07 18:18:28 -0600

    Gnome sort
    definicion e implementacion

--- GnomeSort.java
+++ GnomeSort.java
@@ -1,30 +1,23 @@
-import java.util.ArrayList;
-import java.util.Collections;
+public class GnomeSort  <T extends Comparable<T>> {
 
-public class GnomeSort <T extends Comparable<T>>{
-    
-	public T[] sort(T[] arr) {
-		int n = arr.length;
-		return gnomeSort(arr, n);
-	}
+    public void sort(T arr[]){
+        int i = 1;
+        while(i < arr.length){
+            if (arr[i].compareTo(arr[i - 1]) >= 0){
+                i++;
 
-	private T[] gnomeSort(T arr[], int n) {
-		int index = 0;
-
-		while (index < n) {
-			if (index == 0)
-				index++;
-			if (arr[index].compareTo(arr[index - 1]) > 0 || arr[index].compareTo(arr[index - 1]) == 0)
-				index++;
-			else {
-				ArrayList<T> helpList = new ArrayList<>(Collections.nCopies(n, null));
-				helpList.add(arr[index]);
-				arr[index] = arr[index - 1];
-				arr[index - 1] = helpList.get(helpList.size() - 1);
-				index--;
-			}
-		}
-		return arr;
-	}
-
+            }
+            else{
+                swap(arr,i,i-1);
+                if(i>2){
+                    i--;
+                }
+            }
+        }
+    }
+    private void swap(T arr[], int i, int j){
+        T tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }
